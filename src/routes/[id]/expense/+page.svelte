b711f28c37a0b11af158fad590af5214dc0d3307
Siddhesh Mangela <sidthecool007@gmail.com>
2025-02-20 15:45:01 +0530

    expense route added

---
+++ src/routes/[id]/expense/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { goto } from '$app/navigation';
+  import { page } from '$app/state';
+  const id = page.params.id;
+
+  $effect(() => {
+    goto(`/${id}`);
+  });
+</script>
