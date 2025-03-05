34a13748cc255fed1249c94a115aeef7bb2eda6e
Siddhesh Mangela <sidthecool007@gmail.com>
2025-03-05 11:45:42 +0530

    layout shifted

--- src/lib/components/Expense/List/ExpenseList/ExpenseGroupItem/ExpenseCard/AmountSection.svelte
+++ src/lib/components/Expense/List/ExpenseList/ExpenseGroupItem/ExpenseCard/AmountSection.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import PaymentModeFormattedOption from '$lib/components/PaymentMode/PaymentModeFormattedOption.svelte';
   import FormattedCurrency from '$lib/components/ui-framework/FormattedInfo/FormattedCurrency.svelte';
   import { useBudgetStore } from '$lib/stores/budget/budget.svelte';
   import type { Expense } from '$lib/stores/expense/types';
@@ -11,9 +12,15 @@
   const currency = $derived(
     useBudgetStore.data.find((item) => item._id === expense.budgetId)?.currency || expense.currency,
   );
+
+  const paymentMode = $derived(
+    useBudgetStore.data.find((item) => item._id === expense.budgetId)?.paymentMode ||
+      expense.paymentMode,
+  );
 </script>
 
 <section class="AmountSection">
+  <PaymentModeFormattedOption {paymentMode} hideLabel />
   <FormattedCurrency value={expense.amount} {currency} />
 </section>
 
@@ -23,5 +30,11 @@
     align-items: center;
     font-weight: 500;
     flex-wrap: wrap;
+
+    :global(.Logo) {
+      width: 30px;
+      height: 30px;
+      font-size: 1.2rem;
+    }
   }
 </style>
