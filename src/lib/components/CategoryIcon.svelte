88dbe4b1f3320fc5dccc4603ec6aeab29b7bc641
Siddhesh Mangela <sidthecool007@gmail.com>
2025-09-15 16:45:10 +0530

    category colored icons added

--- src/lib/components/CategoryIcon.svelte
+++ src/lib/components/CategoryIcon.svelte
@@ -11,21 +11,21 @@
 
 <span class="CategoryIcon">
   {#if category === Category.ENTERTAINMENT}
-    <Icon icon="fa7-solid:masks-theater" width="24" height="24" />
+    <Icon icon="fa7-solid:masks-theater" width="24" height="24" class="ENTERTAINMENT" />
   {:else if category === Category.FLIGHT}
-    <Icon icon="mdi:flight" width="24" height="24" />
+    <Icon icon="mdi:flight" width="24" height="24" class="FLIGHT" />
   {:else if category === Category.FOOD}
-    <Icon icon="mdi:food" width="24" height="24" />
+    <Icon icon="mdi:food" width="24" height="24" class="FOOD" />
   {:else if category === Category.MART}
-    <Icon icon="mdi:cart" width="24" height="24" />
+    <Icon icon="mdi:cart" width="24" height="24" class="MART" />
   {:else if category === Category.SHOPPING}
-    <Icon icon="mdi:shopping" width="24" height="24" />
+    <Icon icon="mdi:shopping" width="24" height="24" class="SHOPPING" />
   {:else if category === Category.STAY}
-    <Icon icon="material-symbols:hotel" width="24" height="24" />
+    <Icon icon="material-symbols:hotel" width="24" height="24" class="STAY" />
   {:else if category === Category.TOUR}
-    <Icon icon="map:boat-tour" width="28" height="28" />
+    <Icon icon="map:boat-tour" width="28" height="28" class="TOUR" />
   {:else if category === Category.TRANSPORT}
-    <Icon icon="ri:taxi-fill" width="24" height="24" />
+    <Icon icon="ri:taxi-fill" width="24" height="24" class="TRANSPORT" />
   {:else}
     <Icon icon="icon-park-solid:other" width="24" height="24" />
   {/if}
@@ -36,5 +36,38 @@
     display: inline-flex;
     justify-content: center;
     align-items: center;
+    color: #8170ff;
+
+    :global(.ENTERTAINMENT) {
+      color: #76a521;
+    }
+
+    :global(.FLIGHT) {
+      color: #d263ef;
+    }
+
+    :global(.FOOD) {
+      color: #ff9a40;
+    }
+
+    :global(.MART) {
+      color: #40a6ff;
+    }
+
+    :global(.SHOPPING) {
+      color: #40a6ff;
+    }
+
+    :global(.STAY) {
+      color: #3bc5a9;
+    }
+
+    :global(.TOUR) {
+      color: #40e6ff;
+    }
+
+    :global(.TRANSPORT) {
+      color: #d1d530;
+    }
   }
 </style>
