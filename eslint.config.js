4093f0b53034087d0dd6cd18b7c27c134742d4ba
Siddhesh Mangela <sidthecool007@gmail.com>
2025-10-03 14:47:50 +0530

    init storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import prettier from 'eslint-config-prettier';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
@@ -37,4 +40,5 @@ export default ts.config(
       },
     },
   },
+  storybook.configs["flat/recommended"]
 );
