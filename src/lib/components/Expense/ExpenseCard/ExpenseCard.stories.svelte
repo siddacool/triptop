81c9b62466d05d936e21d952899a1f1982fef091
Siddhesh Mangela <sidthecool007@gmail.com>
2025-10-03 17:36:30 +0530

    expense fixed

---
+++ src/lib/components/Expense/ExpenseCard/ExpenseCard.stories.svelte
@@ -0,0 +1,75 @@
+<script module lang="ts">
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import ExpenseCard from './ExpenseCard.svelte';
+  import { Category, PaymentModes } from '$lib/stores/expense/individual.svelte';
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    component: ExpenseCard,
+    tags: ['autodocs'],
+    args: {
+      data: {
+        id: 1,
+        _id: 'exp_001',
+        createdAt: Date.now(),
+        updatedAt: Date.now(),
+        name: 'Dinner at Italian Restaurant',
+        amount: 45.5,
+        tripId: 'trip_123',
+        category: Category.FOOD,
+        date: new Date('2025-09-25').getTime(),
+        paymentMode: PaymentModes.CASH,
+        currency: 'USD',
+        tags: JSON.stringify(['dinner', 'restaurant', 'italian']),
+      },
+    },
+  });
+</script>
+
+<Story name="Default" />
+
+<Story
+  name="INR"
+  args={{
+    data: {
+      id: 1,
+      _id: 'exp_001',
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      name: 'Jwellary',
+      amount: 100000,
+      tripId: 'trip_123',
+      category: Category.SHOPPING,
+      date: new Date('2025-09-25').getTime(),
+      paymentMode: PaymentModes.CASH,
+      currency: 'INR',
+    },
+  }}
+/>
+
+<Story
+  name="CARD"
+  args={{
+    data: {
+      id: 1,
+      _id: 'exp_001',
+      createdAt: Date.now(),
+      updatedAt: Date.now(),
+      name: 'Dinner at Italian Restaurant',
+      amount: 45.5,
+      tripId: 'trip_123',
+      category: Category.FOOD,
+      date: new Date('2025-09-25').getTime(),
+      paymentMode: PaymentModes.CARD,
+      currency: 'USD',
+      tags: JSON.stringify(['dinner', 'restaurant', 'italian']),
+    },
+  }}
+/>
+
+<Story
+  name="details"
+  args={{
+    details: true,
+  }}
+/>
