4111ea120d212fc317a197f9ebf60631697b52de
Siddhesh Mangela <sidthecool007@gmail.com>
2026-06-17 12:30:42 +0530

    debug page added

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,28 +5,11 @@
   import '@flightlesslabs/dodo-ui-bits/styles/main.scss';
   // Addon (dodo-ui-date)
   import '@flightlesslabs/dodo-ui-date/styles/main.css';
-  import { ModalManager, ToastManager } from '@flightlesslabs/dodo-ui-bits';
-  import { Theme } from '@flightlesslabs/dodo-ui';
-  import TheHeader from '$lib/components/TheHeader/TheHeader.svelte';
-  import Main from '$lib/components/Main/Main.svelte';
 
   let { children } = $props();
 </script>
 
-<Theme global class="Theme" type="auto">
-  <div class="App">
-    <TheHeader />
-
-    <Main>
-      <div class="box">
-        {@render children()}
-      </div>
-    </Main>
-  </div>
-
-  <ToastManager clearAfterDuration={3000} alignmentY="bottom" alignmentX="center" limit={3} />
-  <ModalManager />
-</Theme>
+{@render children()}
 
 <style lang="scss">
   :global(*) {
@@ -37,34 +20,5 @@
     font-family: 'Zalando Sans', sans-serif;
     font-optical-sizing: auto;
     font-size: 16px;
-  }
-
-  :global(body) {
-    margin: 0;
-    height: 100vh;
-    overflow: hidden;
-  }
-
-  :global(.Theme) {
-    height: 100vh;
-    overflow: hidden;
-  }
-
-  .App {
-    display: flex;
-    height: 100%;
-    width: 100%;
-    flex-direction: column;
-  }
-
-  .box {
-    width: 100%;
-    max-width: 800px;
-    margin: 0 auto;
-    padding: calc(var(--dodo-ui-space) * 2);
-
-    @media (min-width: 600px) {
-      padding: calc(var(--dodo-ui-space) * 3);
-    }
   }
 </style>
---
+++ src/routes/trips/+layout.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+  import { ModalManager, ToastManager } from '@flightlesslabs/dodo-ui-bits';
+  import { Theme } from '@flightlesslabs/dodo-ui';
+  import TheHeader from '$lib/components/TheHeader/TheHeader.svelte';
+  import Main from '$lib/components/Main/Main.svelte';
+
+  let { children } = $props();
+</script>
+
+<Theme global class="Theme" type="auto">
+  <div class="App">
+    <TheHeader />
+
+    <Main>
+      <div class="box">
+        {@render children()}
+      </div>
+    </Main>
+  </div>
+
+  <ToastManager clearAfterDuration={3000} alignmentY="bottom" alignmentX="center" limit={3} />
+  <ModalManager />
+</Theme>
+
+<style lang="scss">
+  :global(body) {
+    margin: 0;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  :global(.Theme) {
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  .App {
+    display: flex;
+    height: 100%;
+    width: 100%;
+    flex-direction: column;
+  }
+
+  .box {
+    width: 100%;
+    max-width: 800px;
+    margin: 0 auto;
+    padding: calc(var(--dodo-ui-space) * 2);
+
+    @media (min-width: 600px) {
+      padding: calc(var(--dodo-ui-space) * 3);
+    }
+  }
+</style>
