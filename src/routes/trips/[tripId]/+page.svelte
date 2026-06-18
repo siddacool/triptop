2eb69b7139b99afcdf44f9ddd94a648a2fde60f0
Siddhesh Mangela <sidthecool007@gmail.com>
2026-06-18 15:38:03 +0530

    Fix Search retention

--- src/routes/trips/[tripId]/+page.svelte
+++ src/routes/trips/[tripId]/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import { beforeNavigate } from '$app/navigation';
   import { page } from '$app/state';
   import TripExpensesSection from '$lib/components/Trips/TripPage/TripExpensesSection/TripExpensesSection.svelte';
   import TripHeader from '$lib/components/Trips/TripPage/TripHeader/TripHeader.svelte';
+  import { useExpenseFiltersStore } from '$lib/stores/expense/filters.svelte';
   import { useExpenseListStore } from '$lib/stores/expense/list.svelte';
   import { useTripStore } from '$lib/stores/trip/individual.svelte';
   import { onMount } from 'svelte';
@@ -14,6 +16,14 @@
     }
 
     useExpenseListStore.fetch(tripId);
+  });
+
+  beforeNavigate((navigation) => {
+    // Ignore refresh/tab close
+    if (!navigation.to) return;
+
+    console.log('Leaving trip page');
+    useExpenseFiltersStore.reset();
   });
 </script>
 
