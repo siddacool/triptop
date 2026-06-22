cc95377f62c973f20024f72470f625bee069cfdb
Siddhesh Mangela <sidthecool007@gmail.com>
2026-06-22 12:12:50 +0530

    Button layouts fixed

---
+++ .changeset/quiet-insects-cover.md
@@ -0,0 +1,5 @@
+---
+'triptop': patch
+---
+
+Button layouts fixed
--- src/lib/components/ui/ControlSection/ControlSection.svelte
+++ src/lib/components/ui/ControlSection/ControlSection.svelte
@@ -6,12 +6,23 @@
     children?: Snippet;
     class?: string;
     controlsAlignment?: ComponentAlignmentX;
+    columnLayout?: boolean;
   };
 
-  const { class: className = '', children, controlsAlignment = 'start' }: Props = $props();
+  const {
+    class: className = '',
+    children,
+    controlsAlignment = 'start',
+    columnLayout = false,
+  }: Props = $props();
 
   const classes = $derived(
-    ['ControlSection', `controlsAlignment--${controlsAlignment}`, className].filter(Boolean),
+    [
+      'ControlSection',
+      `controlsAlignment--${controlsAlignment}`,
+      columnLayout ? 'columnLayout' : '',
+      className,
+    ].filter(Boolean),
   );
 </script>
 
@@ -22,59 +33,87 @@
 <style lang="scss">
   .ControlSection {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
     width: 100%;
-
-    @media (min-width: 600px) {
-      flex-direction: row;
-      margin-left: calc(var(--dodo-ui-space) * -1);
-      margin-right: calc(var(--dodo-ui-space) * -1);
-    }
+    margin-left: calc(var(--dodo-ui-space) * -1);
+    margin-right: calc(var(--dodo-ui-space) * -1);
 
     &.controlsAlignment--start {
-      align-items: flex-start;
-      justify-content: center;
-
-      @media (min-width: 600px) {
-        align-items: center;
-        justify-content: flex-start;
-      }
+      align-items: center;
+      justify-content: flex-start;
     }
 
     &.controlsAlignment--center {
       align-items: center;
       justify-content: center;
-
-      @media (min-width: 600px) {
-        align-items: center;
-        justify-content: center;
-      }
     }
 
     &.controlsAlignment--end {
-      align-items: flex-end;
-      justify-content: center;
-
-      @media (min-width: 600px) {
-        align-items: center;
-        justify-content: flex-end;
-      }
+      align-items: center;
+      justify-content: flex-end;
     }
 
     :global(.dodo-ui-Button) {
-      width: 100%;
-      margin-bottom: calc(var(--dodo-ui-space) * 2);
+      width: initial;
+      margin: 0 var(--dodo-ui-space);
+      margin-bottom: 0;
+    }
 
-      &:last-child {
-        margin-bottom: 0;
-      }
+    &.columnLayout {
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      margin-left: 0;
+      margin-right: 0;
 
       @media (min-width: 600px) {
-        width: initial;
-        margin: 0 var(--dodo-ui-space);
-        margin-bottom: 0;
+        flex-direction: row;
+        margin-left: calc(var(--dodo-ui-space) * -1);
+        margin-right: calc(var(--dodo-ui-space) * -1);
+      }
+
+      &.controlsAlignment--start {
+        align-items: flex-start;
+        justify-content: center;
+
+        @media (min-width: 600px) {
+          align-items: center;
+          justify-content: flex-start;
+        }
+      }
+
+      &.controlsAlignment--center {
+        align-items: center;
+        justify-content: center;
+
+        @media (min-width: 600px) {
+          align-items: center;
+          justify-content: center;
+        }
+      }
+
+      &.controlsAlignment--end {
+        align-items: flex-end;
+        justify-content: center;
+
+        @media (min-width: 600px) {
+          align-items: center;
+          justify-content: flex-end;
+        }
+      }
+
+      :global(.dodo-ui-Button) {
+        width: 100%;
+        margin-bottom: calc(var(--dodo-ui-space) * 2);
+
+        &:last-child {
+          margin-bottom: 0;
+        }
+
+        @media (min-width: 600px) {
+          width: initial;
+          margin: 0 var(--dodo-ui-space);
+          margin-bottom: 0;
+        }
       }
     }
   }
