b7b53b6a63731492ed79ff28a8627eb818bb6712
Siddhesh Mangela <sidthecool007@gmail.com>
2026-06-24 12:17:30 +0530

    Settings page fixed

---
+++ .changeset/six-pillows-brake.md
@@ -0,0 +1,5 @@
+---
+'triptop': patch
+---
+
+Settings page fixed
--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -1,21 +1,11 @@
 <script>
   import SettingsCard from '$lib/components/Settings/SettingsCard/SettingsCard.svelte';
   import PageHeadingNav from '$lib/components/ui/PageHeadingNav/PageHeadingNav.svelte';
-  import { useTripListStore } from '$lib/stores/trip/list.svelte';
-
-  import { onMount } from 'svelte';
-
-  onMount(() => {
-    useTripListStore.fetch();
-  });
 </script>
 
 <svelte:head>
-  <title>Trips</title>
+  <title>Settings</title>
 </svelte:head>
 
 <PageHeadingNav href="/trips">Settings</PageHeadingNav>
 <SettingsCard />
-
-<style lang="scss">
-</style>
