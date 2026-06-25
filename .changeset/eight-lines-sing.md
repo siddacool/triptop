565b70456987f366e9dc8d353d5cf0077b7ab03e
Siddhesh Mangela <sidthecool007@gmail.com>
2026-06-25 10:16:15 +0530

    Privacy policy added

---
+++ .changeset/eight-lines-sing.md
@@ -0,0 +1,5 @@
+---
+'triptop': patch
+---
+
+Privacy policy added
---
+++ src/routes/privacy-policy/+page.svelte
@@ -0,0 +1,16 @@
+<div>
+  <h2>Privacy Policy for Triptop</h2>
+
+  <p>
+    This application is Free and Open Source software distributed under the <b>MIT License</b>.
+    Copyright (c) 2026 Siddhesh Mangela.
+  </p>
+
+  <p>
+    The complete source code is fully transparent and publicly accessible at
+    <a href="https://github.com/siddacool/triptop">triptop grithub</a>
+  </p>
+</div>
+
+<style lang="scss">
+</style>
